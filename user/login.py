290fe0126c4b65d31ed0e09afce9e530fb692bec
rollky <45863206+rollky@users.noreply.github.com>
2024-09-28 11:34:55 +0800

    Update login.py

--- user/login.py
+++ user/login.py
@@ -12,7 +12,7 @@ try:
     else:
         QLMain = '/ql'
 except:
-    QLMain = '/ql/data'
+    QLMain = '/ql'
 
 if BOT.get('proxy_user') and BOT['proxy_user'] != "代理的username,有则填写，无则不用动":
     proxy = {
